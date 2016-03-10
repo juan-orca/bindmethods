2082d05a69030e577ae06d59e33d717a624a22ef
Juan <juan@orcadefense.com>
2016-03-10 13:51:52 -0500

    Add readme

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = function bindMethods(methods, that) {
-  console.log("hi world")
-  //for(var i = 0; i < methods.length; i++) {
-  //  that[methods[i]] = that[methods[i]].bind(that);
-  //}
+  for(var i = 0; i < methods.length; i++) {
+    that[methods[i]] = that[methods[i]].bind(that);
+  }
 }

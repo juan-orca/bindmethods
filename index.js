49fee96c42456071ee5b828e1dd3d9b1e69f84fa
Juan <juan@orcadefense.com>
2016-03-10 13:15:38 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = function bindMethods(methods, that) {
+  console.log("hi world")
+  //for(var i = 0; i < methods.length; i++) {
+  //  that[methods[i]] = that[methods[i]].bind(that);
+  //}
+}
